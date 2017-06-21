38d87770f9339899997aae17257f6c3320458931
Josh Arce <josh.arce@outlook.com>
2017-06-20 23:37:38 -0700

    Initial Commit

---
+++ scripts/portfolio.js
@@ -0,0 +1,45 @@
+var mediumWindowSize = window.matchMedia("(min-width: 768px)");
+
+$('.navbar-nav a').on('click', function (event) {
+    $('.navbar-nav a').removeClass('nav-item-active');
+    $(this).addClass('nav-item-active');
+    /*if (!mediumWindowSize.matches) {
+        $('.navbar-toggler').click();
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 250
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    } else {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            /*$('html, body').animate({
+                scrollTop: $(hash).offset().top - 75
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    }*/
+});
+
+if (!mediumWindowSize.matches) {
+    $('.devicons i').addClass('colored');
+    $('#name').html('<br />JOSH ARCE');
+} else {
+    $('.about-me').on('mouseenter mouseleave', function() {
+        $('.devicons i').toggleClass('colored');
+    });
+}
+
+$(window).resize(function() {
+    if (!mediumWindowSize.matches) {
+        $('#name').html('<br />JOSH ARCE');
+    } else {
+        $('#name').html('JOSH ARCE');
+    }
+});
